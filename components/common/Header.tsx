0f1dff0ce9e68f4e581252ede74771a6414ac3e5
manak1 <mana23happy@hotmail.co.jp>
2021-01-03 06:07:00 +0900

    feat: リンク先の指定

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,18 +1,19 @@
-import React from "react"
+import Link from "next/link";
+import React from "react";
 
 const Header: React.FC = () => {
   return (
     <header className="bg-white w-full">
-    <div className="flex items-center justify-between mx-auto container px-4 py-2">
-      <h1 className="font-bold text-lg">
-        身長診断メーカー
-      </h1>
-      <a href="#">
-        診断を作る
-      </a>
-    </div>
-  </header>
-  )
-}
+      <div className="flex items-center justify-between mx-auto container px-4 py-2">
+        <Link href="/">
+          <a href="#">
+            <h1 className="font-bold text-lg">身長診断メーカー</h1>
+          </a>
+        </Link>
+        <a href="#">診断を作る</a>
+      </div>
+    </header>
+  );
+};
 
-export  default Header;
+export default Header;
