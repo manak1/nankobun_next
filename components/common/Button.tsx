9873ddcd85348e5c6c7f199b6cbe99589501d856
manak1 <mana23happy@hotmail.co.jp>
2021-01-17 23:23:25 +0900

    style: Buttonにpropsの追加

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -6,6 +6,8 @@ import { css } from "@emotion/react";
 type Props = {
   href?: string;
   children: string;
+  rounded?: boolean;
+  widthFull?: boolean;
   onClick?: () => void;
 };
 
@@ -13,14 +15,30 @@ const buttonColor = css({
   backgroundColor: "#26a6ff;",
 });
 
-const Button: React.FC<Props> = ({ children, href, onClick }) => {
+const changeRoundedClass = (rounded: boolean) => {
+  return rounded ? "rounded-3xl" : "";
+};
+
+const changeWidthClass = (widthFull: boolean) => {
+  return widthFull ? "w-full" : "rounded-md";
+};
+
+const Button: React.FC<Props> = ({
+  children,
+  href,
+  onClick,
+  rounded,
+  widthFull,
+}) => {
   return (
     <span className="text-center">
       {href ? (
         <Link href={href}>
           <a
             href="#"
-            className="inline-block mx-auto bg-blue-500 text-white hover:opacity-75 py-2 w-32 text-center shadow rounded-md"
+            className={`inline-block mx-auto bg-blue-500 text-white hover:opacity-75 py-2 w-32 text-center shadow  ${changeRoundedClass(
+              rounded
+            )}`}
           >
             {children}
           </a>
@@ -29,7 +47,9 @@ const Button: React.FC<Props> = ({ children, href, onClick }) => {
         <a
           href="#"
           onClick={onClick}
-          className="inline-block mx-auto bg-blue-500 text-white py-2 w-32 hover:opacity-75 text-center shadow rounded-md"
+          className={`inline-block mx-auto bg-blue-500 text-white py-2 w-32 hover:opacity-75 text-center shadow  ${changeRoundedClass(
+            rounded
+          )} ${changeWidthClass(widthFull)}`}
         >
           {children}
         </a>
