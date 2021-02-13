9fd0189b2f1f43519d67f829f9191652c43e4abf
manak1 <mana23happy@hotmail.co.jp>
2021-02-13 16:39:12 +0900

    style: デザイン調整

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React from "react";
+import Button from "../common/Button";
 
 const Header: React.FC = () => {
   return (
     <header className="bg-blue-300 shadow-xl w-full container mx-auto border-b border-l border-r">
-      <div className="flex items-center justify-center mx-auto container px-4 py-2">
+      <div className="flex items-center justify-between mx-auto container px-4 py-2">
         <Link href="/">
           <a href="#">
             <h1 className="font-bold text-xl text-white">
@@ -12,6 +13,9 @@ const Header: React.FC = () => {
             </h1>
           </a>
         </Link>
+        <Button href="/shindan/create" rounded>
+          診断を作る
+        </Button>
       </div>
     </header>
   );
--- components/common/Ogp.tsx
+++ components/common/Ogp.tsx
@@ -35,20 +35,22 @@ const Ogp: React.FC<Props> = ({ shindanData }) => {
   return (
     <div
       css={ogpBox}
-      className="p-1 w-4/5 py-6 relative flex flex-col bg-blue-400 text-center mx-auto rounded text-white"
+      className="p-1 w-4/5 pt-5 pb-6 relative flex flex-col bg-blue-400 text-center mx-auto rounded text-white"
     >
       <div css={ogpTop}>
-        <h2 className="font-bold text-2xl">あなたの身長は</h2>
-        <p className="font-bold text-2xl">
-          <span className="mr-1 text-3xl">{shindanData.emoji.native}</span>何
-          {shindanData.unit}分?
+        <h2 className="font-bold text-xl sm:text-2xl">あなたの身長は</h2>
+        <p className="font-bold text-xl sm:text-2xl">
+          <span className="mr-1 text-2xl sm:text-3xl">
+            {shindanData.emoji.native}
+          </span>
+          何{shindanData.unit}分?
         </p>
       </div>
-      <div css={ogpBottom}>
+      <div className="mt-2" css={ogpBottom}>
         <a
           href="#"
           css={buttonCta}
-          className="inline-block text-xs mx-auto font-bold pointer-events-none bg-blue-500 mt-2 text-white py-2 w-24 text-center shadow rounded-md"
+          className="inline-block text-xs mx-auto font-bold pointer-events-none bg-blue-500 text-white py-2 w-24 text-center shadow rounded-md"
         >
           診断する
         </a>
