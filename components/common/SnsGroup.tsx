a14f106526d8e7927a57ede1aaccdd3647c64998
manak1 <mana23happy@hotmail.co.jp>
2021-02-14 12:08:30 +0900

    feat: 各SNSのシェアボタンを追加

--- components/common/SnsGroup.tsx
+++ components/common/SnsGroup.tsx
@@ -1,52 +1,61 @@
 import React from "react";
-import ButtonSns from "../common/ButtonSns"
-const SnsGroup: React.FC = ()=> {
+import ButtonSns from "../common/ButtonSns";
 
-  type sns =  {
-    name: string,
-    href: string,
-    icon: string,
-    bgColor: string
-  }
-  type snsList = sns[]
+type Props = {
+  docId: string;
+};
+
+type sns = {
+  name: string;
+  href: string;
+  icon: string;
+  bgColor: string;
+};
+
+type snsList = sns[];
+
+const SnsGroup: React.FC<Props> = ({ docId }) => {
+  const productionUrl = "https://shincyo-shindan.vercel.app";
 
   const lists: snsList = [
     {
-      name: 'Twitter',
-      href: 'aaa',
-      icon: '/static/images/sns/facebook-brands.svg',
-      bgColor: "#3c94d8"
+      name: "Twitter",
+      href: `https://twitter.com/intent/tweet?url=${productionUrl}/shindan/${docId}`,
+      icon: "/static/images/sns/twitter-brands.svg",
+      bgColor: "#3c94d8",
     },
     {
-      name: 'FaceBook',
-      href: 'aaa',
-      icon: '/static/images/sns/facebook-brands.svg',
-      bgColor: "#3d5897"
+      name: "FaceBook",
+      href: `http://www.facebook.com/share.php?u=${productionUrl}/shindan/${docId}`,
+      icon: "/static/images/sns/facebook-brands.svg",
+      bgColor: "#3d5897",
     },
     {
-      name: 'Line',
-      href: 'aaa',
-      icon: '/static/images/sns/facebook-brands.svg',
-      bgColor: "#01c300"
+      name: "Line",
+      href: `https://social-plugins.line.me/lineit/share?url=${productionUrl}/shindan/${docId}`,
+      icon: "/static/images/sns/line-brands.svg",
+      bgColor: "#01c300",
     },
-    
-  ]
+  ];
 
   return (
-    <ul className="flex space-x-2 mt-12 justify-center">
-        {
-          lists.map((sns)=> {
-            return (
-              <li key={sns.name}>
-              <ButtonSns href={sns.href} bgColor={sns.bgColor} name={sns.name} icon={sns.icon} >
-                {sns.name}
-              </ButtonSns>
-              </li>
-            )
-          })
-        }
+    <ul className="flex space-x-2 mt-8 justify-center">
+      {lists.map((sns) => {
+        return (
+          <li key={sns.name}>
+            <ButtonSns
+              href={sns.href}
+              bgColor={sns.bgColor}
+              name={sns.name}
+              icon={sns.icon}
+            >
+              {sns.name}
+            </ButtonSns>
+          </li>
+        );
+      })}
     </ul>
-  )
-}
+  );
+};
 
 export default SnsGroup;
